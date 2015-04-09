e691be384b85453976cff44aea25343920205c00
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-04-09 12:02:58 +0900

    draw scatter plot

--- murase/parallel_job/visualize_runs/run.rb
+++ murase/parallel_job/visualize_runs/run.rb
@@ -4,6 +4,9 @@ class Run
     @place = run_hash["placeId"]
     @start_at = run_hash["startAt"]
     @finish_at = run_hash["finishAt"]
+
+    @parameters = { beta: run_hash["beta"], h: run_hash["h"] }
+    @results = { order_parameter: run_hash["result"] }
   end
 
   def self.set_scale( num_places, min_start_at, max_finish_at )
@@ -25,6 +28,17 @@ class Run
 
     $app.fill(255)
     $app.rect( x0, y0, x1-x0, y1-y0, 10 )
+  end
+
+  def draw_scatter_plot( range = { beta: [0.1,0.5], h:[-1.0, -0.6] } )
+    x,y = range.map do |key, (min,max)|
+      (@parameters[key] - min).to_f / (max - min)
+    end
+    x *= $app.width
+    y *= $app.height
+
+    $app.fill(255)
+    $app.ellipse( x, y, 10, 10 )
   end
 end
 
