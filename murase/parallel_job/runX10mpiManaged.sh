44e14dd671d66dc45a4ced2da7fc6b047e2f15b1
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-05-28 17:58:14 +0900

    update job script to run mock

--- murase/parallel_job/runX10mpiManaged.sh
+++ murase/parallel_job/runX10mpiManaged.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 #============ pjsub Options ============
-#PJM --rsc-list "node=4"
-#PJM --rsc-list "elapse=00:05:00"
+#PJM --rsc-list "node=12"
+#PJM --rsc-list "elapse=00:10:00"
 #PJM --stg-transfiles all
 #PJM --mpi "use-rankdir"
-#PJM --mpi "proc=32"
 #PJM --stgin-dir "rank=* ./build %r:./"
+#PJM --stgin  "rank=* ./mock/dummy_simulator.py %r:./"
 #PJM --stgin  "rank=0 /data/ra000014/a03144/local/icedCopy.tar 0:../"
 #PJM --stgin-dir  "rank=* /data/ra000014/a03144/local/x10.dist.stageIn %r:./x10"
 #PJM --stgout-dir "rank=* %r:./ %j"
 #PJM -s
 
 #============ Shell Script ============
+#  ## #PJM --mpi "proc=12"
 
 . /work/system/Env_base
 
 tar xf ../icedCopy.tar -C ../
 
-export X10_NPLACES=32
-export X10_NTHREADS=1
+export X10_NPLACES=12
+export X10_NTHREADS=2
 export X10RT_MPI_ENABLE_COLLECTIVES=true
 
 ICED_COPY=../icedCopy
@@ -29,10 +30,8 @@ export JAVA_HOME=$ICED_COPY
 export LD_LIBRARY_PATH=$ICED_COPY/jre/lib/s64fx/server:.:$X10D:$LD_LIBRARY_PATH
 export _JAVA_OPTIONS="-Xmx2048m -Xss16m"
 
-PROGRAM=Main
-ARG=task_list.txt
+PROGRAM=Mock
+ARG=12345
 
-ls
-
-mpiexec $X10D/X10MPIJava -ea -Djava.library.path=$LD_LIBRARY_PATH -Djava.class.path=.:$X10D/x10.jar:$X10D/commons-math3-3.3.jar:$X10D/commons-logging-1.2.jar:$X10D/hazelcast-3.3.jar -Djava.util.logging.config.file=$X10D/logging.properties -DX10RT_IMPL=mpi $PROGRAM\$\$Main $ARG
+mpiexec $X10D/X10MPIJava -ea -Djava.library.path=$LD_LIBRARY_PATH -Djava.class.path=.:$X10D/x10.jar:$X10D/commons-math3-3.3.jar:$X10D/commons-logging-1.2.jar:$X10D/hazelcast-3.3.jar:./json-simple-1.1.1.jar -Djava.util.logging.config.file=$X10D/logging.properties -DX10RT_IMPL=mpi $PROGRAM\$\$Main $ARG
 
