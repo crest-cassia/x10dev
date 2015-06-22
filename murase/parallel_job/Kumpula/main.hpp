2b2fc9151151b0df62209ca44d754b2d6f4131c6
Yohsuke Murase <yohsuke.murase@gmail.com>
2015-06-22 19:36:35 +0900

    add simulator files

---
+++ murase/parallel_job/Kumpula/main.hpp
@@ -0,0 +1,23 @@
+#ifndef MAIN_KUMPULA
+#define MAIN_KUMPULA
+
+int RunSimulator(
+    int net_size,
+    double p_tri,
+    double p_jump,
+    double delta,
+    double p_nd,
+    double p_ld,
+    double aging,
+    double w_th,
+    double alpha,
+    double mobility,
+    double noise,
+    double p_flight,
+    double beta,
+    int t_max,
+    int seed
+  );
+
+#endif // MAIN_KUMPULA
+
