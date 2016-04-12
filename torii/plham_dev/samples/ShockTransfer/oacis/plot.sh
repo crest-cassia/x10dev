ab67fd3f3dee8849fdca537c224c3e9f7cf4cbbf
Takuma Torii <torii@localhost>
2016-04-12 14:53:07 +0900

    .

--- torii/plham_dev/samples/ShockTransfer/oacis/plot.sh
+++ torii/plham_dev/samples/ShockTransfer/oacis/plot.sh
@@ -3,9 +3,9 @@ die() { echo "$@"; exit 1; }
 
 home=${0%oacis/plot.sh} #'samples/ShockTransfer'
 
-[ $# -ge 2 ] || die "$0: Need more than 2 arguments but [$@]
-Usage: $ bash $home/oacis/plot.sh plot.R output.png"
+[ $# -ge 1 ] || die "$0: Need more than 1 arguments but [$@]
+Usage: $ bash $home/oacis/plot.sh output.png"
 
-[ -f "$1" ] || die "$0: $1 must be R script file"
+[ -n "$1" ] || die "$0: $1 must be an non-empty string"
 
-Rscript "$1" $(find _input -name '_stdout.txt') $2
+Rscript "$home"/plot.R $(find _input -name '_stdout.txt') "$1"
